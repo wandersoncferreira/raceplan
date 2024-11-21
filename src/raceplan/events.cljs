3186331a407322feb015c783b485a38763601d74
Wanderson Ferreira <wand@hey.com>
2024-11-21 17:03:14 -0300

    complete next line

--- src/raceplan/events.cljs
+++ src/raceplan/events.cljs
@@ -29,8 +29,15 @@
    (let [{:keys [splits]} db
          [row split] val
          row-split (get splits (first row) {})
-         new-row-split (assoc row-split row split)]
-     (assoc-in db [:splits (first row)] new-row-split))))
+         new-row-split (assoc row-split row split)
+         maybe-next-index (when (= "1" (second row))
+                            (str (inc (js/parseInt (first row)))))
+         maybe-next-value (when maybe-next-index split)
+         new-db (assoc-in db [:splits (first row)] new-row-split)]
+     (if maybe-next-index
+       (assoc-in new-db [:splits maybe-next-index]
+                 {(str maybe-next-index "0") maybe-next-value})
+       new-db))))
 
 (re-frame/reg-event-db
  ::del-row-number
